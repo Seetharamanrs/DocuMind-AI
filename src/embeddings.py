8131667529b4e24b4c9dd1d53e0a6483597776ad
seetharamanrs <seetharaman2632001@gmail.com>
2026-07-10 15:34:12 +0530

    The model is intiated outside the function for easy understanding

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,13 +1,14 @@
 from sentence_transformers import SentenceTransformer
 from src.document_loader import load_pdf
 from src.text_splitter import split_text
-def generate_embeddings(chunks):
-    model=SentenceTransformer("models/all-MiniLM-L6-v2")
+def generate_embeddings(model,chunks):
+    
     chunks_embedding=model.encode(chunks)
     return chunks_embedding
 
 if __name__=="__main__":
     file_path=r"D:\my_git\DocuMind-AI\data\policy document.pdf"
+    model=SentenceTransformer("models/all-MiniLM-L6-v2")
     text=load_pdf(file_path)
     chunks=split_text(text)
     generate_embeddings(chunks)
