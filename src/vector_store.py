ce5e30f39dee45b68dd022395fee024b5f878dbe
seetharamanrs <seetharaman2632001@gmail.com>
2026-07-14 19:27:28 +0530

    By saving the vector and chunk of the pdf we can retrieve the chunk quicker by comparing the vector

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,8 +1,7 @@
 import faiss
+import pickle
 import numpy as np
-from src.text_splitter import split_text
-from src.document_loader import load_pdf
-from src.embeddings import generate_embeddings
+
 
 def create_vector_store(chunks_embeddings):
     embeddings=np.array(chunks_embeddings).astype("float32")
@@ -10,10 +9,29 @@ def create_vector_store(chunks_embeddings):
     index=faiss.IndexFlatL2(dimension)
     index.add(embeddings)
     return index
-if __name__=="__main__":
-    file_path=r"D:\my_git\DocuMind-AI\data\policy document.pdf"
-    text=load_pdf(file_path)
-    chunks=split_text(text)
-    chunks_embeddings=generate_embeddings(chunks)
-    index = create_vector_store(chunks_embeddings)
-    print(index.ntotal)
+def save_vector_store(index):
+    faiss.write_index(index,"vector_store/faiss_index.bin")
+
+def load_vector_store():
+    index = faiss.read_index("vector_store/faiss_index.bin")
+    return index
+
+
+def save_chunks(chunks):
+    with open("vector_store/chunks.pkl", "wb") as f:
+        pickle.dump(chunks, f)
+
+
+def load_chunks():
+    with open("vector_store/chunks.pkl", "rb") as f:
+        chunks = pickle.load(f)
+    return chunks 
+
+
+# if __name__=="__main__":
+#     file_path=r"D:\my_git\DocuMind-AI\data\policy document.pdf"
+#     text=load_pdf(file_path)
+#     chunks=split_text(text)
+#     chunks_embeddings=generate_embeddings(chunks)
+#     index = create_vector_store(chunks_embeddings)
+#     print(index.ntotal)
