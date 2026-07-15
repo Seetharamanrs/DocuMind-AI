f572770a90b4481852fe8d69e5a58f21a99bd813
seetharamanrs <seetharaman2632001@gmail.com>
2026-07-15 15:52:05 +0530

    The changes is added

--- src/document_loader.py
+++ src/document_loader.py
@@ -1,16 +1,11 @@
 from pypdf import PdfReader
 def load_pdf(file_path):
+   
     reader=PdfReader(file_path)
     text=""
     for page in reader.pages:
         text+=page.extract_text()
     return text
-
-file_path=r"D:\my_git\DocuMind-AI\data\policy document.pdf"
-
 if __name__=="__main__":
+    file_path=r"D:\my_git\DocuMind-AI\data\policy document.pdf"
     load_pdf(file_path)
- 
-
-
-
